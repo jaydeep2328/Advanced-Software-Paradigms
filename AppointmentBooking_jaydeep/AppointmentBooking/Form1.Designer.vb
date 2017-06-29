d2eb07b33a4755bd9252697b7eaa10cfc0a66d67
jaydeep2328 <jaydeep.j.chopde@gmail.com>
2017-06-29 17:02:11 -0400

    Add files via upload

---
+++ AppointmentBooking_jaydeep/AppointmentBooking/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dates
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(19, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(210, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(19, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Select Date"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(19, 135)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(210, 23)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(15, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Time Slot"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(19, 175)
+        Me.TextBox2.MaxLength = 180
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(324, 93)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(19, 159)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(161, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Reason for Appointment"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(255, 296)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "yyyy-MM-dd"
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(22, 86)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(207, 21)
+        Me.DateTimePicker1.TabIndex = 9
+        Me.DateTimePicker1.Value = New Date(2017, 3, 20, 8, 17, 15, 0)
+        '
+        'Dates
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(418, 325)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Dates"
+        Me.Text = "Booking Window"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+
+End Class
---
+++ AppointmentBooking_jaydeep/AppointmentBooking/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Dates
+    Dim myconn As New MySqlConnection("Server=localhost;User Id=root;Password=030889;Database=appointments")
+    Dim command As New MySqlCommand
+    Dim command1 As New MySqlCommand
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        myconn = New MySqlConnection("Server=localhost;User Id=root;Password=030889;Database=appointments")
+        Dim myread As MySqlDataReader
+        Try
+            myconn.Open()
+            Dim query As String
+            query = "insert into appointments.schedule(name,datebooked,timebooked,reason) values('" & TextBox1.Text & "','" & DateTimePicker1.Text & "','" & ComboBox1.Text & "','" & TextBox2.Text & "')"
+            command = New MySqlCommand(query, myconn)
+            myread = command.ExecuteReader
+            Dim confirmation As String = "Your appointment has been booked on " & DateTimePicker1.Text & " for " & ComboBox1.Text
+            MsgBox(confirmation)
+            myconn.Close()
+            Me.Refresh()
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message)
+        Finally
+            myconn.Dispose()
+
+        End Try
+        Me.Close()
+    End Sub
+    
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+        myconn = New MySqlConnection("Server=localhost;User Id=root;Password=030889;Database=appointments")
+        Dim myread As MySqlDataReader
+        DateTimePicker1.MinDate = Now
+        Dim all_slots(9) As String
+        all_slots(0) = "09:00-10:00"
+        all_slots(1) = "10:00-11:00"
+        all_slots(2) = "11:00-12:00"
+        all_slots(3) = "12:00-13:00"
+        all_slots(4) = "13:00-14:00"
+        all_slots(5) = "14:00-15:00"
+        all_slots(6) = "15:00-16:00"
+        all_slots(7) = "16:00-17:00"
+        all_slots(8) = "17:00-18:00"
+        all_slots(9) = "18:00-19:00"
+
+
+        Dim avail_slots As New List(Of String)()
+        Dim booked_slots As New List(Of String)()
+       
+        Try
+            myconn.Open()
+
+            Dim query As String
+            query = "Select * from schedule where datebooked='" & DateTimePicker1.Text & "'"
+            command = New MySqlCommand(query, myconn)
+            myread = command.ExecuteReader
+            While myread.Read
+                booked_slots.Add(myread.GetString("timebooked"))
+                If booked_slots.Count = 10 Then
+                    MsgBox("All slots for the selected date are booked.Please select other date")
+                End If
+            End While
+            myconn.Close()
+
+            For i As Integer = 0 To all_slots.Length - 1
+                If Not booked_slots.Contains(all_slots(i)) Then
+                    avail_slots.Add(all_slots(i))
+                    
+                End If
+                
+            Next
+            ComboBox1.Items.Clear()
+
+            For j As Integer = 0 To avail_slots.Count - 1
+                ComboBox1.Items.Add(avail_slots.Item(j))
+                
+            Next
+
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message)
+        Finally
+            myconn.Dispose()
+
+        End Try
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
